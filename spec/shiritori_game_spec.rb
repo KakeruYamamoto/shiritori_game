c1020ca7b35cdaf546b199b110f8377fbfe5eb36
kakeru <okinawan.kkr0713@icloud.com>
2019-11-18 17:19:50 +0900

    spec/shiritori_game_spec.rb 修正

--- spec/shiritori_game_spec.rb
+++ spec/shiritori_game_spec.rb
@@ -3,11 +3,7 @@ require './shiritori_game'
 RSpec.describe ShiritoriGame do
   describe '.rule' do
     it 'シリトリのルールが表示される' do
-      expect(ShiritoriGame.rule).to eq(puts(%Q|
-        すべてカタカナで入力してください
-        最後に 'ン' がついたら負けです
-        同じ単語を使ったら負けです
-      |))
+      expect(ShiritoriGame.rule).to eq("すべてカタカナで入力してください\n最後に 'ン' がついたら負けです\n同じ単語を使ったら負けです\n")
     end
   end
 end
