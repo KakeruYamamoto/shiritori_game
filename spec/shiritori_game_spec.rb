e7b3675d1225463322fa19a1663de58f57527f5f
kakeru <okinawan.kkr0713@icloud.com>
2019-11-18 17:39:44 +0900

    spec/shiritori_game_spec.rb に#start追記

--- spec/shiritori_game_spec.rb
+++ spec/shiritori_game_spec.rb
@@ -6,8 +6,14 @@ RSpec.describe ShiritoriGame do
       expect(ShiritoriGame.rule).to eq("すべてカタカナで入力してください\n最後に 'ン' がついたら負けです\n同じ単語を使ったら負けです\n")
     end
   end
-  
+
   describe '.all_words' do
    it { expect(ShiritoriGame.all_words).to eq ["リンゴ", "ゴリラ", "ラッパ", "パリ"] }
  end
+ 
+ describe '#start' do
+    subject(:shiritori_game) { ShiritoriGame.new(player_name: 'シリトリプレイヤー') }
+    it { expect(shiritori_game.start).to include("\\_o< Hi, シリトリプレイヤー. Let's enjoy shiritori game\n") }
+  end
+
 end
